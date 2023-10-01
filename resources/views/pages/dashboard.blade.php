860aeb07a98078d9fa2520362557cde489a7ef52
Md___Emon <ishtiaqueferdous@nextgenitltd.com>
2023-10-01 12:11:41 +0600

    New Design Added

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,79 +1,114 @@
-@extends('welcome')
+@extends('master')
 
 @push('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
     <style>
-        /* Prevent text from wrapping */
-        .btn-edit-link {
-            white-space: nowrap;
-        }
-        .card-dashboard {
-            border: none;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-            transition: box-shadow 0.3s ease-in-out;
+        body {
+            background: linear-gradient(to right, #8e9eab, #eef2f3);
+            font-family: Arial, sans-serif;
         }
 
-        .card-dashboard:hover {
-            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+        .container {
+            max-width: 100%;
+            margin-top: 50px;
+            background: rgba(255, 255, 255, 0.8);
+            padding: 40px;
+            border-radius: 15px;
         }
 
-        .card-header-dashboard {
-            background-color: #007bff;
-            color: #fff;
+        .stat-card {
+            background: linear-gradient(to left, #a1c4fd, #c2e9fb);
+            color: white;
+            border-radius: 10px;
             text-align: center;
-            font-size: 24px;
+        }
+
+        .stat-title {
+            font-size: 20px;
+            margin: 20px 0;
+        }
+
+        .stat-number {
+            font-size: 36px;
+            font-weight: bold;
+        }
+
+        td {
+            word-wrap: break-word;
+            max-width: 150px;
+            /* or whatever maximum width you want to set */
         }
     </style>
 @endpush
 
 @section('content')
-    <div class="card-dashboard">
-        <div class="card-header-dashboard">
-            Shortening Link Statistics
+    <div class="container mt-5">
+        <h1>Dashboard</h1>
+
+        <!-- Statistics -->
+        <div class="row mt-4">
+            <div class="col-md-6">
+                <div class="card stat-card">
+                    <div class="card-body">
+                        <div class="stat-title">Shortened Links</div>
+                        <div class="stat-number" id="shortenedCount">{{ $totalGeneratedLink }}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card stat-card">
+                    <div class="card-body">
+                        <div class="stat-title">Total Clicks</div>
+                        <div class="stat-number" id="clickCount">{{ $totalClick }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            <table id="example" class="hover" style="width: 100% !important;">
-                <thead>
+
+        <!-- Data Table -->
+        <h3 class="mt-5">Links</h3>
+        <table id="dataTable" class="table table-striped table-bordered">
+            <thead>
                 <tr>
                     <th>Original Link</th>
-                    <th>Shorten Link</th>
-                    <th>Clicked</th>
+                    <th>Shortened Link</th>
+                    <th>Clicks</th>
                     <th>Action</th>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($datas as $index => $item)
+            </thead>
+            <tbody id="tableBody">
+                <!-- Data will go here -->
+                @foreach ($datas as $index => $item)
                     <tr>
                         <td>{{ $item->original_link }}</td>
-                        <td id="shortLink">{{ env('APP_URL')."/".$item->shortening_link }}</td>
+                        <td id="shortLink">{{ env('APP_URL') . '/' . $item->shortening_link }}</td>
                         <td>{{ $item->click[0]->count }}</td>
                         <td>
-                                <button type="button" class="btn btn-primary edit-link btn-edit-link" data-toggle="modal" data-target="#exampleModalCenter" data-id="{{ $item->id }}">
-                                    Edit Short Link
-                                </button>
+                            <button type="button" class="btn btn-primary edit-link btn-edit-link" data-toggle="modal"
+                                data-target="#exampleModalCenter" data-id="{{ $item->id }}">
+                                Edit Short Link
+                            </button>
                         </td>
                     </tr>
                 @endforeach
-                </tbody>
-                <tfoot>
-                <tr>
-                    <th>Original Link</th>
-                    <th>Shorten Link</th>
-                    <th>Clicked</th>
-                    <th>Action</th>
-                </tr>
-                </tfoot>
-            </table>
-            @include('pages.modals.edit')
-        </div>
+            </tbody>
+        </table>
+        @include('pages.modals.edit')
     </div>
 @endsection
 
 @push('js')
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
     <script>
-        new DataTable('#example');
+        $(document).ready(function() {
+            // Initialize DataTable
+            $('#dataTable').DataTable();
+        });
     </script>
     <script>
         $(document).ready(function() {
@@ -84,10 +119,9 @@
                 $('#linkId').val(linkId);
             });
 
-            $('#exampleModalCenter').on('hidden.bs.modal', function () {
+            $('#exampleModalCenter').on('hidden.bs.modal', function() {
                 $('#linkId').val('');
             });
         });
     </script>
-
 @endpush
